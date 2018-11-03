c0857cdfcf0b0aaf3b3d0cf977fcbd8d9c38079c
hwendland <wendland.hannah@gmail.com>
2018-11-03 19:28:55 +0100

    CBild with import and export
    Changed CBild to be initialized with the key that is supposed to be used
    for encoding.
    CBild does NOT inherit from NBild but has methods to import an NBild and
    export arbitrary NBild objects to file, so we can export the original as well as
    the encoded image.
    Encode and decode should now work with whatever file has been imported
    and use the key the object has been initialised with.

--- cbild.cpp
+++ cbild.cpp
@@ -1,6 +1,22 @@
 #include "cbild.h"
 
-CBild::CBild()
-{
+void CBild::import(const string filename) {
+    NBild image = NBild();
+    image.import(filename);
+    this->originalImage = image;
+}
 
+void CBild::writeToFile(NBild imageToWrite, string outfile) {
+    imageToWrite.writeToFile(outfile);
+}
+
+void CBild::encode() {
+    NBild encodedImage = NBild();
+    //do something with this->originalImage based on this->key and save result in this->encodedImage?
+    this->encodedImage = encodedImage;
+}
+
+void CBild::decode() {
+    NBild decodedImage = NBild();
+    //do something with this->encodedImage based on this->key
 }
--- cbild.h
+++ cbild.h
@@ -1,11 +1,27 @@
 #ifndef CBILD_H
 #define CBILD_H
+#include "nbild.h"
 
 
 class CBild
 {
+    vector<vector<int>> A = {{0,1},{1,0}};
+    vector<vector<int>> B = {{1,0},{0,1}};
+
 public:
-    CBild();
+    CBild(char key) {
+        this->key = key;
+    }
+
+    char key;
+    NBild originalImage;
+    NBild encodedImage;
+
+    void import(const string filename);
+    void writeToFile(NBild imageToWrite, string outfile);
+
+    void encode();
+    void decode();
 };
 
 #endif // CBILD_H
